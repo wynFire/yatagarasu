0c63557929ed52fb67579de11e2182c5527e99a6
Daisy.Wu <Daisy.Wu@audiencescience.com>
2016-10-14 13:58:17 +0800

    addd

--- util/src/main/java/com/yatagarasu/util/thread/ConcurrentReq.java
+++ util/src/main/java/com/yatagarasu/util/thread/ConcurrentReq.java
@@ -1,5 +1,20 @@
 package com.yatagarasu.util.thread;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ConcurrentReq {
-	int a;
+  private static final int THREAD_NUM = 5;
+  private static final int MISSION_NUM = 10;
+
+  public static void main(String[] args) {
+    ExecutorService pool = Executors.newFixedThreadPool(THREAD_NUM);
+    for (int i = 0; i < MISSION_NUM; i++) {
+      MissionThread mission = new MissionThread();
+      pool.execute(mission);
+    }
+    
+    pool.shutdown();
+  }
+
 }
